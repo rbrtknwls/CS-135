aec836addbac7a2995a52d51c7581275692288e5
rbrtknwls <r2knowle@uwaterloo.ca>
2020-11-23 07:48:53 -0500

    thrid part of 2A!

--- ao8/redux.rkt
+++ ao8/redux.rkt
@@ -16,11 +16,13 @@
 ;; =================================
 
 
+
 ;; (parity bstring) Produces the if the given string
-;;    (bstring) contains an even amount of 1s
+;;    (bstring) contains an even amount of 1s or odd
+;;    amounts of 1s.
 ;; Examples:
-;(check-expect (parity "1110") 'odd)
-;(check-expect (parity "Hello123") 'odd)
+(check-expect (parity "1110") 'odd)
+(check-expect (parity "Hello123") 'odd)
 (check-expect (parity "00110011") 'even)
 
 ;; parity: Str -> Bool
@@ -36,22 +38,136 @@
 
 
 ;; Basic Tests
-;(check-expect (parity "") 'even)
-;(check-expect (parity "11011011") 'even)
-;(check-expect (parity "1") 'odd)
-;(check-expect (parity "2") 'even)
-;(check-expect (parity "11") 'even)
-;(check-expect (parity "1") 'odd)
-;(check-expect (parity "2") 'even)
-;(check-expect (parity "11") 'even)
+(check-expect (parity "") 'even)
+(check-expect (parity "11011011") 'even)
+(check-expect (parity "1") 'odd)
+(check-expect (parity "2") 'even)
+(check-expect (parity "11") 'even)
+(check-expect (parity "1") 'odd)
+(check-expect (parity "2") 'even)
+(check-expect (parity "11") 'even)
 
 
 ;; Letters and Numbers Mixed
-;(check-expect (parity "1Hihowareyou2") 'odd)
-;(check-expect (parity "Hi211") 'even)
-;(check-expect (parity "01100111Hi") 'odd)
-;(check-expect (parity "") 'even)
-;(check-expect (parity "hi") 'even)
+(check-expect (parity "1Hihowareyou2") 'odd)
+(check-expect (parity "Hi211") 'even)
+(check-expect (parity "01100111Hi") 'odd)
+(check-expect (parity "") 'even)
+(check-expect (parity "hi") 'even)
+
+
+
+;; =================================
+;;
+;; Question 2B
+;;
+;; =================================
+
+
+
+;; (replace-word indexstr replacstr strlist) Produces a
+;;   new list in which all occurrences in a string list
+;;   (strlist) of a string (indexstr) are replaced by
+;;   a different string (replacstr)
+;; Examples:
+(check-expect
+ (replace-word "exam" "assessment"
+               (cons "content"
+                     (cons "exam"
+                           (cons "assignment" empty))))
+ (cons "content" (cons "assessment" (cons "assignment" empty))))
+(check-expect
+ (replace-word "oranges" "apples"
+               (cons "oranges"
+                     (cons "orang"
+                           (cons "apples" empty))))
+ (cons "apples" (cons "orang" (cons "apples" empty))))
+
+;; replace-word: Str Str (listof Str) -> (listof Str)
+(define (replace-word indexstr replacstr strlist)
+  (map (lambda (x) (cond [(string=? x indexstr) replacstr]
+                         [else                          x]))
+       strlist))
+
+;; General Tests
+(check-expect (replace-word "apple" "orange" '("apples"))
+              '("apples"))
+(check-expect (replace-word "pen" "pencil" '("pens" "pens"))
+              '("pens" "pens"))
+(check-expect (replace-word "Jack" "Jill" '("Jack" "Jack Jack"))
+              '("Jill" "Jack Jack"))
+(check-expect (replace-word "abc" "efg" '("abcde" "abcd" "abc"))
+              '("abcde" "abcd" "efg"))
+(check-expect (replace-word "hello." "Hello!!"
+                           '("hello." "Hello?" "hello"))
+              '("Hello!!" "Hello?" "hello"))
+
+;; Empty List Test
+(check-expect (replace-word "Calc" "No" empty)
+              empty)
+
+;; Multiple Replacements / Zero Replacements
+(check-expect (replace-word "Name" "Robbie" '("Name?"))
+              '("Name?"))
+(check-expect (replace-word "Name" "Robbie" '("Name"))
+              '("Robbie"))
+(check-expect (replace-word "Name" "Robbie"
+                            '("What" "is" "your" "name?"))
+              '("What" "is" "your" "name?"))
+(check-expect (replace-word "Name" "Robbie"
+                            '("Name" "Name" "Name" "Name"))
+              '("Robbie" "Robbie" "Robbie" "Robbie"))
+
+
+
+;; =================================
+;;
+;; Question 2C
+;;
+;; =================================
+
+
+
+;; (all-factors num) Produces a the list of natural (nat) factors
+;;   of a given number (num), that divide evenly into that number
+;; Examples:
+(check-expect
+ (all-factors 30)
+  (cons 1 (cons 2 (cons 3 (cons 5
+  (cons 6 (cons 10 (cons 15 empty))))))))
+(check-expect
+ (all-factors 10)
+  (cons 1 (cons 2 (cons 5 empty))))
+
+;; all-factors: Nat -> (listof Nat)
+(define (all-factors num)
+  (foldr (lambda (x rorr)
+                (cond [(empty? x) rorr]
+                      [else   (append (list x) rorr)]))
+         empty
+         (build-list num (lambda (x)
+                    (cond [(= x 0)             empty]
+                          [(= (remainder num x) 0) x]
+                          [else                empty])))))
+
+;; Basic Tests
+(check-expect (all-factors 16)  (list 1 2 4 8))
+(check-expect (all-factors 14)  (list 1 2 7))
+(check-expect (all-factors 63)  (list 1 3 7 9 21))
+(check-expect (all-factors 50)  (list 1 2 5 10 25))
+(check-expect (all-factors 30)  (list 1 2 3 5 6 10 15))
+(check-expect (all-factors 18)  (list 1 2 9))
+
+;; Zero/One Test
+(check-expect (all-factors 0)  empty)
+(check-expect (all-factors 1)  empty)
+
+;; Prime Tests
+(check-expect (all-factors 2)  (list 1))
+(check-expect (all-factors 7)  (list 1))
+(check-expect (all-factors 13) (list 1))
+
+
 
 
 
