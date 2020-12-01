ecdfb8ad603be3bf1726c4d9ba200be254457a6a
rbrtknwls <r2knowle@uwaterloo.ca>
2020-11-30 23:03:36 -0500

    -f Edge Case
    Added more Tests
    Checked for starting with a closing bracket (stack would be empty)

--- ao9/brackets.rkt
+++ ao9/brackets.rkt
@@ -18,46 +18,44 @@
 
 ;; balanced?: Str -> Bool
 (define (balanced? bstring)
+
   (cond [(empty? bstring) true]
-        [else (compare-brack
-               (string->list bstring) empty)]))
+        [else 
+          (local [
+            ;; (compare-brack blist bstack) Given a string of
+            ;;  brackets, compares if for each element in b string
+            ;;  its corrisponding value will exists within the bstack
 
-
-;; =================================
-;; Helper Functions
-;; =================================
-
-
-;; (compare-brack blist bstack) Given a string of brackets, compares
-;;  if for each element in b string its corrisponding value will exist
-;;  within the bstack
-;; Examples:
-(check-expect (compare-brack (list #\)) (list #\()) true)
-(check-expect (compare-brack (list #\)) (list #\<)) false)
-(check-expect (compare-brack (list #\( #\< #\> #\)) empty) true)
-(check-expect (compare-brack (list #\( #\[ #\> #\)) empty) false)
-
-;; compare-brack: (listof Str) (listof Str) -> Bool
-(define (compare-brack blist bstack)
-   (cond [(and (empty? blist) (empty? bstack)) true]
-         [(empty? blist)                      false]
-         [(char=? (first blist) #\))
-             (and (char=? (first bstack) #\()
-                  (compare-brack (rest blist)
-                                 (rest bstack)))]
-         [(char=? (first blist) #\>)
-             (and (char=? (first bstack) #\<)
-                  (compare-brack (rest blist)
-                                 (rest bstack)))]
-         [(char=? (first blist) #\])
-             (and (char=? (first bstack) #\[)
-                  (compare-brack (rest blist)
-                                 (rest bstack)))]
-         [else
-             (compare-brack
-                (rest blist)
-                (append (list (first blist))
-                        bstack))]))
+            ;; compare-brack: (listof Str) (listof Str) -> Bool
+            
+                  (define (compare-brack blist bstack)
+                    (cond [(and (empty? blist) (empty? bstack)) true]
+                          [(empty? blist)                      false]
+                          [(and (empty? bstack)
+                                (or (char=? (first blist) #\))
+                                    (char=? (first blist) #\>)
+                                    (char=? (first blist) #\])))
+                                                               false]
+                          [(char=? (first blist) #\))
+                           (and (char=? (first bstack) #\()
+                                (compare-brack (rest blist)
+                                               (rest bstack)))]
+                          [(char=? (first blist) #\>)
+                           (and (char=? (first bstack) #\<)
+                                (compare-brack (rest blist)
+                                               (rest bstack)))]
+                          [(char=? (first blist) #\])
+                           (and (char=? (first bstack) #\[)
+                                (compare-brack (rest blist)
+                                               (rest bstack)))]
+                          [else
+                           (compare-brack
+                            (rest blist)
+                            (append (list (first blist))
+                                    bstack))]))]
+            
+                (compare-brack (string->list bstring)
+                               empty))]))
 
 
 ;; =================================
@@ -77,5 +75,9 @@
 (check-expect (balanced? "<[()][()()]>[]") true)
 (check-expect (balanced? "<[[]]()>") true)
 (check-expect (balanced? "<[[]()(<>)]>") true)
+(check-expect (balanced? "([]<>)") true)
+(check-expect (balanced? ">>>><<<<") false)
+(check-expect (balanced? "))[]()") false)
+(check-expect (balanced? "][)[") false)
 
 
